f82107f5e61bf68cdf0347cf941a58863657522d
Pedro <pedropacheco92@outlook.com>
2018-06-14 21:04:37 -0300

    front end

--- front-end/locadora/src/app/app.module.ts
+++ front-end/locadora/src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
+import { CarsComponent } from './page/cars/cars.component';
+import { CarComponent } from './page/car/car.component';
+
+const appRoutes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'main' }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    CarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true } // <-- debugging purposes only
+    ),
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
