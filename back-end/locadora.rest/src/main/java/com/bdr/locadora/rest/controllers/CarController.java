440b9b4615a2177a53d043bf092d32f0bdeaa217
Pedro Henrique Pacheco <pedropacheco92@outlook.com>
2018-06-18 10:33:22 -0300

    status http

--- back-end/locadora.rest/src/main/java/com/bdr/locadora/rest/controllers/CarController.java
+++ back-end/locadora.rest/src/main/java/com/bdr/locadora/rest/controllers/CarController.java
@@ -1,5 +1,9 @@
 package com.bdr.locadora.rest.controllers;
 
+import static java.util.Objects.nonNull;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -9,6 +13,8 @@ import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,8 +39,9 @@ public class CarController {
 	 * @return
 	 */
 	@RequestMapping(path = "", method = POST, consumes = APPLICATION_JSON_UTF8_VALUE)
-	public Car create(@RequestBody Car car) {
-		return this.carService.create(car);
+	public ResponseEntity<Car> create(@RequestBody Car car) {
+		// return the created car with status 201 (created)
+		return new ResponseEntity<>(carService.create(car), CREATED);
 	}
 
 	/**
@@ -45,18 +52,24 @@ public class CarController {
 	 */
 	@RequestMapping(path = "/{id}", method = PUT, consumes = APPLICATION_JSON_UTF8_VALUE)
 	public Car update(@PathVariable Long id, @RequestBody Car car) {
-		return this.carService.update(id, car);
+		return carService.update(id, car);
 	}
 
 	/**
-	 * Load a car by Id
+	 * Load a car by Id If doesen't find a car by id, return blank with status 404
 	 *
 	 * @param id
 	 * @return Car
 	 */
 	@RequestMapping(path = "/{id}", method = GET)
-	public Car load(@PathVariable Long id) {
-		return this.carService.load(id);
+	public ResponseEntity<Car> load(@PathVariable Long id) {
+		Car car = carService.load(id);
+		if (nonNull(car)) {
+			// if contains car, return it with status 200 (0k)
+			return new ResponseEntity<>(car, OK);
+		}
+		// else, return status 404 (not found)
+		return new ResponseEntity<>(NOT_FOUND);
 	}
 
 	/**
@@ -66,18 +79,22 @@ public class CarController {
 	 */
 	@RequestMapping(path = "", method = GET)
 	public Collection<Car> loadAll() {
-		return this.carService.loadAll();
+		return carService.loadAll();
 	}
 
 	/**
-	 * Load a car by Id
+	 * Delete a car by Id, returns no content status
 	 *
 	 * @param id
-	 * @return Car
 	 */
 	@RequestMapping(path = "/{id}", method = DELETE)
-	public boolean delete(@PathVariable Long id) {
-		return this.carService.delete(id);
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		if (carService.delete(id)) {
+			// if deleted return a successful status but with no content
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		// else, return status 404 (not found)
+		return new ResponseEntity<>(NOT_FOUND);
 	}
 
 }
